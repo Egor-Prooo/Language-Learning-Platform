5dab83046daacf259c356c07d178b8416ee0f0cd
Egor-Prooo <egor.prosyanov.highschool@buditel.bg>
2025-12-16 21:42:52 +0200

    Implemented Courses amd Lessons controller

---
+++ LanguageLearningPlatform/Controllers/CoursesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LanguageLearningPlatform.Web.Controllers
+{
+    public class CourseController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ LanguageLearningPlatform/Controllers/LessonsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LanguageLearningPlatform.Web.Controllers
+{
+    public class LessonsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

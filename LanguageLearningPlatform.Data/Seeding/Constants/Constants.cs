d72368558f69635ba590d60c21eb83e4952d736f
Egor-Prooo <egor.prosyanov.highschool@buditel.bg>
2025-12-16 21:27:29 +0200

    Implemented Progress and Course services

---
+++ LanguageLearningPlatform.Data/Seeding/Constants/Constants.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LanguageLearningPlatform.Data.Seeding.Constants
+{
+    internal class Constants
+    {
+    }
+}

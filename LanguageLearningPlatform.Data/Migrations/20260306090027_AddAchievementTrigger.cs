06b1a6b4e8c513f729cd97a37fb795dab1f1e920
Egor-Prooo <egor.prosyanov.highschool@buditel.bg>
2026-03-06 11:02:51 +0200

    Applied migration for updated Achievements table

---
+++ LanguageLearningPlatform.Data/Migrations/20260306090027_AddAchievementTrigger.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LanguageLearningPlatform.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAchievementTrigger : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "TriggerType",
+                table: "Achievements",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "TriggerValue",
+                table: "Achievements",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TriggerType",
+                table: "Achievements");
+
+            migrationBuilder.DropColumn(
+                name: "TriggerValue",
+                table: "Achievements");
+        }
+    }
+}

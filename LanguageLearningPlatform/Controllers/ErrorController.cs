55e4682bf56bf17ee39af9ce37c7d7987786dfc6
Egor-Prooo <egor.prosyanov.highschool@buditel.bg>
2026-03-19 12:46:00 +0200

    Added ErrorController and custom pages for error 403, 404 and 500

---
+++ LanguageLearningPlatform/Controllers/ErrorController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LanguageLearningPlatform.Web.Controllers
+{
+    public class ErrorController : Controller
+    {
+        [Route("Error/{statusCode}")]
+        public IActionResult HttpStatusCodeHandler(int statusCode)
+        {
+            var statusCodeResult = HttpContext.Features.Get<IStatusCodeReExecuteFeature>();
+
+            ViewBag.OriginalPath = statusCodeResult?.OriginalPath ?? "";
+
+            return statusCode switch
+            {
+                404 => View("Error404"),
+                403 => View("Error403"),
+                _ => View("Error500")
+            };
+        }
+
+        [Route("Error")]
+        public IActionResult Index()
+        {
+            var exceptionDetails = HttpContext.Features.Get<IExceptionHandlerPathFeature>();
+            ViewBag.ExceptionPath = exceptionDetails?.Path ?? "";
+            return View("Error500");
+        }
+    }
+}
